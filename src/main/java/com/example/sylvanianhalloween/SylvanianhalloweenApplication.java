e2011d0842e9f4e769dbcc4062df8c67dfd2e0dd
mii1004 <mii.1004@docomo.ne.jp>
2021-10-30 22:41:32 +0900

    first commit

---
+++ src/main/java/com/example/sylvanianhalloween/SylvanianhalloweenApplication.java
@@ -0,0 +1,13 @@
+package com.example.sylvanianhalloween;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SylvanianhalloweenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SylvanianhalloweenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sylvanianhalloween/SylvanianhalloweenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sylvanianhalloween;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SylvanianhalloweenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
